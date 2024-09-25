cbb4986be6234d6eafae98fcebf2d73c24faaf92
Muskan-Tiwari <muskanttiwari2004@gmail.com>
2024-09-25 15:51:38 +0530

    Update program2.py

--- program2.py
+++ program2.py
@@ -1,10 +1,25 @@
 class Solution(object):
     def romanToInt(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        pass
+        roman={
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        total=0
+        previous=0
+        for char in reversed(s):
+            currentValue=roman[char]
+            if currentValue<previous:
+                total+=currentValue
+            else:
+                total+=currentValue
+                previous=currentValue
+        print(total)
+            
 
 
 
