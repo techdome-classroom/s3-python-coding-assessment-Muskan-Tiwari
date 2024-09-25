10a77d4992d3f30e547d6baa89feb7b54de35cfc
Muskan-Tiwari <muskanttiwari2004@gmail.com>
2024-09-25 15:53:49 +0530

    Update program1.py

--- program1.py
+++ program1.py
@@ -1,14 +1,16 @@
-def isValid(s):
-    stack=[]
-    bracket={')' : '(' : '}' : '{' : ']' : '['}
-    for char in s:
-        if char in bracket.values():
-            stack.append(char)
-        elif char in bracket.keys():
-            if not stack or stack.pop()!= bracket[char]:
-                print(False)
-                return 
-    print(len(stack)==0)
+class Solution(object):
+    def isValid(self,s):
+     stack=[]
+     bracket={')' : '(' : '}' : '{' : ']' : '['}
+     for char in s:
+         if char in bracket.values():
+             stack.append(char)
+         elif char in bracket.keys():
+             if not stack or stack.pop()!= bracket[char]:
+                 print(False)
+                 return 
+     print(len(stack)==0)
+
 
 
 
