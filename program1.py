ca825329b6f7503644f8be4b670559d9f1913e12
Muskan-Tiwari <muskanttiwari2004@gmail.com>
2024-09-25 15:37:10 +0530

    Update program1.py

--- program1.py
+++ program1.py
@@ -1,10 +1,13 @@
-class Solution(object):
-    def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        pass
+def isValid(s):
+    stack=[]
+    bracket={')' : '(' : '}' : '{' : ']' : '['}
+    for char in s:
+        if char in bracket.values():
+            stack.append(char)
+        elif char in bracket.keys():
+            if not stack or stack.pop()!= bracket[char]:
+                return false
+    return len(stack)==0
 
 
 
